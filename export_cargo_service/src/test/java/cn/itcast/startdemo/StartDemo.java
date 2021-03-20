d5c76444d419de0617122d93fd14aea43acbf831
lc960623 <5309994+lc960623@user.noreply.gitee.com>
2021-03-20 15:34:26 +0800

    提交代码

--- export_cargo_service/src/test/java/cn/itcast/startdemo/StartDemo.java
+++ export_cargo_service/src/test/java/cn/itcast/startdemo/StartDemo.java
@@ -2,7 +2,7 @@ package cn.itcast.startdemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class StartDemo {
+public class StartDemo  {
 
     public static void main(String[] args) throws Exception {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath*:spring/applicationContext-*.xml");
